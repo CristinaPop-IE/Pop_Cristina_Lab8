6b12a0efb2083ca604be2eab7384dde42995823e
Cristina Pop <cristinapop_2000@yahoo.com>
2021-11-28 23:00:12 +0200

    Add project files.

---
+++ Data/Pop_Cristina_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Pop_Cristina_Lab8.Models;
+
+namespace Pop_Cristina_Lab8.Data
+{
+    public class Pop_Cristina_Lab8Context : DbContext
+    {
+        public Pop_Cristina_Lab8Context (DbContextOptions<Pop_Cristina_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pop_Cristina_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Pop_Cristina_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Pop_Cristina_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Pop_Cristina_Lab8.Data;
+using Pop_Cristina_Lab8.Models;
+
+namespace Pop_Cristina_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Pop_Cristina_Lab8.Data.Pop_Cristina_Lab8Context _context;
+
+        public IndexModel(Pop_Cristina_Lab8.Data.Pop_Cristina_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Models.Book> Book { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book
+            .Include(b => b.Publisher)
+            .ToListAsync();
+        }
+    }
+}
